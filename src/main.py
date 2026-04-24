4df9235ee832e73931afd123991bf1c17c3f96ae
Beatriz Caitano <bcaitano06@gmail.com>
2026-04-24 01:24:31 -0300

    feat: adiciona leitura do sensor DS18B20

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("Teste")
+from machine import Pin
+import onewire
+import ds18x20
+import time
+
+ds_pin = Pin(21)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+sensores_conectados = ds_sensor.scan()
+print("Sensores encontrados: ", sensores_conectados)
+while True :
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+
+  for sensor_conectado in sensores_conectados:
+    temperatura = ds_sensor.read_temp(sensor_conectado)
+    print("Temperatura:", temperatura, "°C")
+
+  time.sleep(1)
