f756709420584c7ce4e587aaf61d01f8d08d7e0b
Beatriz Caitano <bcaitano06@gmail.com>
2026-04-25 01:12:48 -0300

    feat: implementa leitura de temperatura com DS18B20 e exibicao em display LCD 16x2 via I2C

--- src/main.py
+++ src/main.py
@@ -1,19 +1,34 @@
-from machine import Pin
+from machine import Pin, SoftI2C
+from i2c_lcd import I2cLcd
 import onewire
 import ds18x20
 import time
 
-ds_pin = Pin(21)
-ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+# CONFIGURAÇÃO DO SENSOR (DS18B20)
+ds_pin = Pin(4) # Define o pino de dados do sensor (barramento OneWire)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin)) # Inicializa o sensor de temperatura
 
-sensores_conectados = ds_sensor.scan()
-print("Sensores encontrados: ", sensores_conectados)
+# CONFIGURAÇÃO DO DISPLAY LCD (I2C)
+i2c = SoftI2C(sda=Pin(21), scl=Pin(22))
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+
+# DETECÇÃO DO SENSORES CONECTADOS E SELEÇÃO DO PRIMEIRO DA LISTA
+sensores_conectados = ds_sensor.scan() 
+sensor_analisado = sensores_conectados[0]
+
+# FORMATAÇÃO DO DISPLAY APOS CONEXAO COM SENSOR CONCLUIDA
+lcd.clear()
+lcd.putstr("Temperatura:")
+lcd.move_to(0, 1)
+lcd.putstr("00.0 C") 
+
+# LOOP PRINCIPAL
 while True :
-  ds_sensor.convert_temp()
-  time.sleep_ms(750)
+  ds_sensor.convert_temp() # Conversão da temperatura do sensor 
+  time.sleep_ms(750) # Tempo de leitura do sensor
 
-  for sensor_conectado in sensores_conectados:
-    temperatura = ds_sensor.read_temp(sensor_conectado)
-    print("Temperatura:", temperatura, "°C")
-
-  time.sleep(1)
+  temperatura = ds_sensor.read_temp(sensor_analisado) # Lê temperatura convertida
+  
+  # Atualiza apenas a segunda linha do display (valor da temperatura) com formatação para evitar residuos
+  lcd.move_to(0, 1)
+  lcd.putstr("{:<16}".format("{:>5.1f} C".format(temperatura)))
