799eae01bb2cb1a050c72e0a0a37f279b0e3f165
bcaitanoo <bcaitano06@gmail.com>
2026-04-27 12:45:37 +0000

    docs: melhora comentários e feedback ao usuário

--- src/main.py
+++ src/main.py
@@ -12,20 +12,26 @@ ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin)) # Inicializa o sensor de te
 i2c = SoftI2C(sda=Pin(21), scl=Pin(22))
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 
-# DETECÇÃO DO SENSORES CONECTADOS E SELEÇÃO DO PRIMEIRO DA LISTA
-sensores_conectados = ds_sensor.scan() 
-sensor_analisado = sensores_conectados[0]
+print("Inicio do programa")
 
-# FORMATAÇÃO DO DISPLAY APOS CONEXAO COM SENSOR CONCLUIDA
+# DETECÇÃO DO SENSORES CONECTADOS E SELEÇÃO DO PRIMEIRO DA LISTA
+sensores_conectados = ds_sensor.scan() # Procura sensores DS18B20 conectados no barramento
+sensor_analisado = sensores_conectados[0] # Seleciona o primeiro sensor encontrado
+print("Sensor conectado: ", sensor_analisado)
+
+# FORMATAÇÃO DO DISPLAY APÓS CONEXãO COM SENSOR CONCLUÍDA
 lcd.clear()
 lcd.putstr("Temperatura:")
 lcd.move_to(0, 1)
 lcd.putstr("00.0 C") 
 
+# CONFIGURAÇÃO DOS LEDS
+
 led_azul = Pin(25, Pin.OUT)
 led_verde = Pin(26, Pin.OUT)
 led_vermelho = Pin(27, Pin.OUT)
 
+# Função responsável por indicar o estado da temperatura via LEDs
 def definir_leds(temp):
     if temp < 10:
         led_azul.value(1)
@@ -46,7 +52,8 @@ while True :
   time.sleep_ms(750) # Tempo de leitura do sensor
 
   temperatura = ds_sensor.read_temp(sensor_analisado) # Lê temperatura convertida
-  definir_leds(temperatura)
+  definir_leds(temperatura)  # Atualiza LEDs conforme a temperatura
+
   # Atualiza apenas a segunda linha do display (valor da temperatura) com formatação para evitar residuos
   lcd.move_to(0, 1)
   lcd.putstr("{:<16}".format("{:>5.1f} C".format(temperatura)))
